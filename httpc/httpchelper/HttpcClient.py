18044605cf236698f9fb07308bcc909a95efcb68
Amanda <amawai@github.com>
2018-09-19 23:33:30 -0400

    [#2] Refactored httpc such that it conforms to the assignment requirements
    - Implemented help functionality
    - Added subparsers for get and post requests, as they require different options
    - Refactored HttpcClient to handle get and post request separately

--- httpc/httpchelper/HttpcClient.py
+++ httpc/httpchelper/HttpcClient.py
@@ -2,33 +2,26 @@ from requests import GetRequest
 
 class HttpcClient:
     @staticmethod
-    def parse_arguments(args): 
+    def execute_get_request(args):
+        url = args.URL
+        headers = args.header or []
+        verbose = False
+        if (args.verbose):
+            verbose = True
+        get_request = GetRequest(url, 80, headers, verbose)
+        get_request.execute()
+    
+    @staticmethod
+    def execute_post_request(args):
+        url = args.URL
         headers = args.header or []
         verbose = False
         if (args.verbose):
             verbose = True
         data = args.data
         file = args.file
-        url = args.URL
-        request = args.request
-
-        if (request.lower() == 'get') :
-            if (data or file):
-                raise ValueError('Flags -f and -d are invalid for GET requests')
-            HttpcClient.execute_get_request(url, headers, verbose)
-        elif (request.lower() == 'post'):
-            if (bool(data) != bool(file)):
-                HttpcClient.execute_post_request(url, headers, verbose, data, file)
-            else:
-                raise ValueError("For POST request, use either -f or -d")
+        if (bool(data) != bool(file)):
+            print('Sending post request')
         else:
-            raise ValueError('Only GET or POST requests are accepted')
-    
-    @staticmethod
-    def execute_get_request(url, headers, verbose):
-        get_request = GetRequest(url, 80, headers, verbose)
-        get_request.execute()
-    
-    @staticmethod
-    def execute_post_request(url, headers, verbose, data, file):
+            raise ValueError("For POST request, use either -f or -d")
         pass
